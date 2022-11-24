cefde6aec24b7f4a8e57f7fa1f3e7823e2474fa9
Charlycruz1721 <87866122+Charlycruz1721@users.noreply.github.com>
2022-11-24 04:00:01 -0600

    Update and rename script.rsc to Print.php

--- Print.php
+++ Print.php
@@ -1,14 +1,5 @@
 
-{
-:local fecha [/system clock get date] ;
-:local hora [/system clock get time] ;
-:local email [/system script get value-name=name [find comment=correo]];
-:local salida ;
-:foreach id in=[/ip route find dst-address=0.0.0.0/0 active ] do={
-:set $swwwalida [/ip route get value-name=gateway-status $id]};
-:local subj "ruta salida";
-:local ody "$salida";
-#/ e-mail send to=albaleonilaj@gmail.com subject="muero por ty" body="$ody";
+
 
 
 
